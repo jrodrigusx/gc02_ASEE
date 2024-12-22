03e2ef56b34b1f04f15c7b39c7f981cf10743494
jrodrigusx <146165440+jrodrigusx@users.noreply.github.com>
2024-12-22 16:25:33 +0100

    Delete API_Visualizaciones directory

--- API_Visualizaciones/swagger_server/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-visualizaciones_blueprint = Blueprint('visualizaciones', __name__)
-
-@visualizaciones_blueprint.route('/some_endpoint', methods=['GET'])
-def example_endpoint():
-    return {"message": "Este es un ejemplo de endpoint de visualizaciones"}
