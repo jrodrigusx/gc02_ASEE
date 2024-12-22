b348147ef4bd8ad3f74f531ec8ba3d44f03be29b
jrodrigusx <146165440+jrodrigusx@users.noreply.github.com>
2024-12-22 16:25:12 +0100

    Delete API_Contenidos directory

--- API_Contenidos/swagger_server/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-contenidos_blueprint = Blueprint('contenidos', __name__)
-
-@contenidos_blueprint.route('/some_endpoint', methods=['GET'])
-def example_endpoint():
-    return {"message": "Este es un ejemplo de endpoint de contenidos"}
