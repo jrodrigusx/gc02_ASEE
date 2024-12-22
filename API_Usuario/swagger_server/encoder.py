61c8b444d159420647c7ddc6eea21b74785b734b
jrodrigusx <146165440+jrodrigusx@users.noreply.github.com>
2024-12-22 16:25:19 +0100

    Delete API_Usuario directory

--- API_Usuario/swagger_server/encoder.py
+++
@@ -1,6 +0,0 @@
-from flask.json import JSONEncoder as FlaskJSONEncoder
-
-class JSONEncoder(FlaskJSONEncoder):
-    def default(self, obj):
-        # Aquí puedes personalizar la serialización de objetos si es necesario
-        return super().default(obj)
